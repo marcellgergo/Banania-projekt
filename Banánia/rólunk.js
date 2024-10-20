09ebae16d0ca3c99faa85847214079472592d164
marcellgergo <71643467+marcellgergo@users.noreply.github.com>
2024-10-20 15:27:57 +0200

    Add files via upload

---
+++ Banánia/rólunk.js
@@ -0,0 +1,6 @@
+document.getElementById('fooldalgomb').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+document.getElementById('tudnivalokgomb').addEventListener('click', function() {
+    window.location.href = 'tudnivalók.html';
+});
