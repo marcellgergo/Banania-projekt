9b5d286f2d32de7d3d9b09db3e23ddc97215cb07
marcellgergo <71643467+marcellgergo@users.noreply.github.com>
2024-10-20 15:27:06 +0200

    Delete Banánia directory

--- Banánia/rólunk.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('fooldalgomb').addEventListener('click', function() {
-    window.location.href = 'index.html';
-});
-document.getElementById('tudnivalokgomb').addEventListener('click', function() {
-    window.location.href = 'tudnivalók.html';
-});
